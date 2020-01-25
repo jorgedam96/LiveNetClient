01b57a2833c7752c5af63b12e4833dd32ac21d57
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2020-01-25 19:36:08 +0100

    mensaje

--- app/src/main/java/com/example/livenet/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/livenet/ui/login/LoginFragment.java
@@ -1,21 +1,19 @@
 package com.example.livenet.ui.login;
 
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.FragmentManager;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.livenet.LoginActivity;
 import com.example.livenet.MainActivity;
