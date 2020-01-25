6fe7a241b618010d9c8fab66185b25790593d17e
jorgedam96 <48628681+jorgedam96@users.noreply.github.com>
2020-01-25 22:13:17 +0100

    prueba marcador con cara

--- app/src/main/java/com/example/livenet/ui/mapas/MapaFragment.java
+++ app/src/main/java/com/example/livenet/ui/mapas/MapaFragment.java
@@ -2,6 +2,16 @@ package com.example.livenet.ui.mapas;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -32,9 +42,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -107,6 +119,9 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback, Google
                 .setFastestInterval(1000); // 1 segundo en milisegundos
 */
 
+
+       //marcardorConCara();
+
     }
 
     private void acercarCamara(Location location) {
@@ -167,6 +182,69 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback, Google
         mMap.setTrafficEnabled(false);
 
 
+    }
+
+
+
+
+
+    private void marcardorConCara() {
+
+
+        LatLng latLng = new LatLng(4.62642, 38.411363);
+        MarkerOptions options = new MarkerOptions().position(latLng);
+        Bitmap bitmap = createUserBitmap();
+        if(bitmap!=null){
+            options.title("Ketan Ramani");
+            options.icon(BitmapDescriptorFactory.fromBitmap(bitmap));
+            options.anchor(0.5f, 0.907f);
+            Marker marker = mMap.addMarker(options);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+        }
+    }
+
+    private Bitmap createUserBitmap() {
+        Bitmap result = null;
+        try {
+            result = Bitmap.createBitmap(dp(62), dp(76), Bitmap.Config.ARGB_8888);
+            result.eraseColor(Color.TRANSPARENT);
+            Canvas canvas = new Canvas(result);
+            Drawable drawable = getResources().getDrawable(R.drawable.livepin);
+            drawable.setBounds(0, 0, dp(62), dp(76));
+            drawable.draw(canvas);
+
+            Paint roundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            RectF bitmapRect = new RectF();
+            canvas.save();
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.avatar);
+            if (bitmap != null) {
+                BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+                Matrix matrix = new Matrix();
+                float scale = dp(52) / (float) bitmap.getWidth();
+                matrix.postTranslate(dp(5), dp(5));
+                matrix.postScale(scale, scale);
+                roundPaint.setShader(shader);
+                shader.setLocalMatrix(matrix);
+                bitmapRect.set(dp(5), dp(5), dp(52 + 5), dp(52 + 5));
+                canvas.drawRoundRect(bitmapRect, dp(26), dp(26), roundPaint);
+            }
+            canvas.restore();
+            try {
+                canvas.setBitmap(null);
+            } catch (Exception e) {}
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        return result;
+    }
+
+    public int dp(float value) {
+        if (value == 0) {
+            return 0;
+        }
+        return (int) Math.ceil(getResources().getDisplayMetrics().density * value);
     }
 
 }
